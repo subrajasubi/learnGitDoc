f7d161cc0f7a2a10d4fae8ebd9ea5547fedf3702
subrajasubi <subrajasubi@gmail.com>
2023-12-27 22:54:14 +0530

    Todays Initial Commit

---
+++ LeafBot/src/main/java/com/leafBot/pages/LoginPage.java
@@ -0,0 +1,53 @@
+package com.leafBot.pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.leafBot.testng.api.base.ProjectSpecificMethods;
+
+
+
+public class LoginPage extends ProjectSpecificMethods{
+	
+	public LoginPage(RemoteWebDriver driver, ExtentTest node){
+		this.driver = driver;
+		this.node = node;
+			}
+	 
+	
+	
+	public LoginPage enterUserName(String data) {	
+		zoomOutUsingJS(150);
+		zoomInUsingJS(70);
+		clearAndType(locateElement("id","username"), data);
+		return this;
+	}	
+
+	public LoginPage enterPassword(String data) {
+		
+	
+		clearAndType(locateElement("id","password"), data);
+		return this;
+	}	
+	
+	public HomePage clickLogin() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		WebElement element = locateElement("class","decorativeSubmit");
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+		driver.executeScript("arguments[0].click();", element);
+		return new HomePage(driver, node);		
+	}
+	
+	
+	public LoginPage verifyErrorMsg(String data) {
+		verifyPartialText(locateElement("id","errordiv"), data);
+		return this;
+	}
+	
+	
+}
